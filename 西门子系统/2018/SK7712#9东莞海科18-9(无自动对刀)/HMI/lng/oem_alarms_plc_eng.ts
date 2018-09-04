6af564ec2f54be1bb9f69454818d49dfa1d119a9
guanzhenhao <kissmonica_gh@163.com>
2018-09-04 15:24:20 +0800

    添加备份

---
+++ 西门子系统/2018/SK7712#9东莞海科18-9(无自动对刀)/HMI/lng/oem_alarms_plc_eng.ts
@@ -0,0 +1,159 @@
+<!DOCTYPE TS>
+<TS>
+  <context>
+    <name>slaeconv</name>
+    <message>
+      <source>700000/PLC/828D</source>
+      <translation>Machine stop, please check the urgent stop button SB1 state！(I24/I2.7)</translation>
+    </message>
+    <message>
+      <source>700001/PLC/828D</source>
+      <translation>Drive temperature alarm！(I33/I4.0)</translation>
+    </message>
+    <message>
+      <source>700002/PLC/828D</source>
+      <translation>Drive not ready！(I32/I3.7)</translation>
+    </message>
+    <message>
+      <source>700003/PLC/828D</source>
+      <translation> 
+Drive to enable unloaded, please load to enable！</translation>
+    </message>
+    <message>
+      <source>700004/PLC/828D</source>
+      <translation>DRF is effective and is interfering with the X1 state manually, please note!</translation>
+    </message>
+    <message>
+      <source>700005/PLC/828D</source>
+      <translation>DRF is effective and is interfering with the X10 state manually, please note!</translation>
+    </message>
+    <message>
+      <source>700006/PLC/828D</source>
+      <translation>DRF is effective and is interfering with the X100 state manually, please note!</translation>
+    </message>
+    <message>
+      <source>700007/PLC/828D</source>
+      <translation>The machine tool X, Z and C axis are not zero, please return to zero operation!</translation>
+    </message>
+    <message>
+      <source>700008/PLC/828D</source>
+      <translation>Machine tool multiplier is zero!</translation>
+    </message>
+    <message>
+      <source>700009/PLC/828D</source>
+      <translation>QF13_ grinding wheel motor  is overloaded！(I27/I3.2)</translation>
+    </message>
+    <message>
+      <source>700010/PLC/828D</source>
+      <translation>ALARM2_ spindle frequency converter alarm！(I28/I3.3)</translation>
+    </message>
+    <message>
+      <source>700011/PLC/828D</source>
+      <translation>QF2_The  hydraulic pump motor  is overloaded!(I11/I1.2)</translation>
+    </message>
+    <message>
+      <source>700012/PLC/828D</source>
+      <translation>QF1_The  intermittent lubrication pump motor  is overloaded！(I9/I1.0)</translation>
+    </message>
+    <message>
+      <source>700013/PLC/828D</source>
+      <translation>KP1_The lubricating oil level of machine tool is too low, please fill it up！(I10/I1.1)</translation>
+    </message>
+    <message>
+      <source>700014/PLC/828D</source>
+      <translation>QF6_The water cooled mechanical motor  is overloaded！(I17/I2.0)</translation>
+    </message>
+    <message>
+      <source>700015/PLC/828D</source>
+      <translation>KP3_Water cooling device cooling water level too low, please add cooling water!(I18/I2.1)</translation>
+    </message>
+    <message>
+      <source>700016/PLC/828D</source>
+      <translation>QF3_The cooling pump motor is overloaded！(I13/I1.4)</translation>
+    </message>
+    <message>
+      <source>700017/PLC/828D</source>
+      <translation>QF4_The flushing pump motor is overloaded！(I14/I1.5)</translation>
+    </message>
+    <message>
+      <source>700018/PLC/828D</source>
+      <translation>QF8_ The coolant tank motor is overloaded！(I21/I2.4)</translation>
+    </message>
+    <message>
+      <source>700019/PLC/828D</source>
+      <translation>KP4_Cooling tank coolant level is too low, please add cooling oil！(I22/I2.5)</translation>
+    </message>
+    <message>
+      <source>700020/PLC/828D</source>
+      <translation>QF9_The dc power supply is overloaded！(I25/I3.0)</translation>
+    </message>
+    <message>
+      <source>700021/PLC/828D</source>
+      <translation>QF10_The control transformer is overloaded！(I26/I3.1)</translation>
+    </message>
+    <message>
+      <source>700022/PLC/828D</source>
+      <translation>QF15_The system drive power is overloaded！(I31/I3.6)</translation>
+    </message>
+    <message>
+      <source>700023/PLC/828D</source>
+      <translation>QF5_The suction spraye motor is overloaded！(I15/I1.6)</translation>
+    </message>
+    <message>
+      <source>700024/PLC/828D</source>
+      <translation>QF7_The air conditioner in the electric box is overloaded！(I19/I2.2)</translation>
+    </message>
+    <message>
+      <source>700025/PLC/828D</source>
+      <translation>ALARM1_The air conditioner in the electric box is not running！(I20/I2.3)</translation>
+    </message>
+    <message>
+      <source>700026/PLC/828D</source>
+      <translation>The machine tool pressure is too low, please check the air way！(I34/I4.1)</translation>
+    </message>
+    <message>
+      <source>700027/PLC/828D</source>
+      <translation>The A axis clamping device is in release state！</translation>
+    </message>
+    <message>
+      <source>700028/PLC/828D</source>
+      <translation>The grinding wheel start-stop button(-SB1) is effective when moving balance！</translation>
+    </message>
+    <message>
+      <source>700029/PLC/828D</source>
+      <translation>Grinding wheel spindle temperature alarm！(I24/I2.7)</translation>
+    </message>
+    <message>
+      <source>700030/PLC/828D</source>
+      <translation>QF14_The roller motor drive is overloaded！(I29/I3.4)</translation>
+    </message>
+    <message>
+      <source>700031/PLC/828D</source>
+      <translation>ALARM3_Roller motor drive alarm！(I30/I3.5)</translation>
+    </message>
+    <message>
+      <source>700032/PLC/828D</source>
+      <translation>ALARM4_Manipulator alarm！(I45/I5.4)</translation>
+    </message>
+    <message>
+      <source>700033/PLC/828D</source>
+      <translation>Please start the hydraulic pump in JOG mode.！</translation>
+    </message>
+    <message>
+      <source>700034/PLC/828D</source>
+      <translation>KP2_The hydraulic oil pressure is too low！(I12/I1.3)</translation>
+    </message>
+    <message>
+      <source>700040/PLC/828D</source>
+      <translation>The different step program 1 output error！</translation>
+    </message>
+    <message>
+      <source>700069/PLC/828D</source>
+      <translation>Grinding wheel spindle or roller motor speed exceeds the allowable range, please check the process parameters！</translation>
+    </message>
+    <message>
+      <source>700074/PLC/828D</source>
+      <translation>Data storage is in progress, please wait for a moment!</translation>
+    </message>
+  </context>
+</TS>
